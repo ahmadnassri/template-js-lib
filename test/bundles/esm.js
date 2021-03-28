94a4aef6882166972025d38f8a55419db055070e
Ahmad Nassri <ahmad@ahmadnassri.com>
2021-03-28 16:23:21 -0400

    style(lint): remove extra "async" keyword

--- test/bundles/esm.js
+++ test/bundles/esm.js
@@ -2,7 +2,7 @@ import { test } from 'tap'
 
 import Bundle from '../..'
 
-test('default import', async assert => {
+test('default import', assert => {
   assert.plan(2)
 
   assert.type(Bundle, Object)
