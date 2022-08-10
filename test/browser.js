69f661223de018a527f86cabcf0e3226de0f7650
Ahmad Nassri <ahmad@ahmadnassri.com>
2022-08-10 00:51:49 +0000

    test: more efficient test run

--- test/browser.js
+++ test/browser.js
@@ -1,4 +1,4 @@
-import tap from 'tap'
+import { test } from 'tap'
 import { fileURLToPath } from 'node:url'
 import { join, dirname } from 'node:path'
 import * as browsers from 'playwright'
@@ -9,31 +9,32 @@ const __dirname = dirname(__filename)
 
 let browser, page
 
-// exit early
-if (['', 'no-browser', undefined].includes(process.env.BROWSER)) process.exit(0)
+const engines = [
+  'chromium',
+  'firefox',
+  'webkit'
+]
 
-console.log(process.env.BROWSER)
+for (const engine of engines) {
+  test(`test with ${engine}`, { skip: process.env.SKIP_BROWSERS === 'true' }, async assert => {
+    browser = await browsers[engine].launch()
+    page = await browser.newPage()
 
-tap.beforeEach(async () => {
-  browser = await browsers[process.env.BROWSER].launch()
-  page = await browser.newPage()
+    await page.goto(`file://${join(__dirname, 'index.html')}`)
+    await page.addScriptTag({
+      path: join(__dirname, '../dist/index.umd.js')
+    })
 
-  await page.goto(`file://${join(__dirname, 'index.html')}`)
-  await page.addScriptTag({
-    path: join(__dirname, '../dist/index.umd.js')
+    assert.plan(1)
+
+    // run in the browser
+    const result = await page.evaluate(() => {
+      const { Lib } = window.TemplateLib
+      return Lib()
+    })
+
+    assert.equal(result, 'hello world')
+
+    await browser.close()
   })
-})
-
-tap.afterEach(() => browser.close())
-
-tap.test('simple test', { skip: !process.env.BROWSER }, async assert => {
-  assert.plan(1)
-
-  // run in the browser
-  const result = await page.evaluate(() => {
-    const { Lib } = window.TemplateLib
-    return Lib()
-  })
-
-  assert.equal(result, 'hello world')
-})
+}
